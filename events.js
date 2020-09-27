465c05578d6bb77656906b11de0d8d06b9e68c95
Ahmad Shela <ahmedsh717@gmail.com>
2020-09-27 22:07:10 +0300

    lab16

---
+++ events.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const Event = require('events');
+/**
+ * this modle is just a global event pool that sherd between all the other files that used event
+ * that make all the project use only one enstance of the Event class
+ */
+module.exports = new Event();
