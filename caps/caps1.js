98ad96336ef5fe230b6ea4b88772e3321ff39d71
Ahmad Shela <ahmedsh717@gmail.com>
2020-10-01 01:47:53 +0300

    fixErrors

--- caps/caps1.js
+++ caps/caps1.js
@@ -22,16 +22,25 @@ connectionPool[id] = socket;
 
 socket.on('data',payload=>{
     let msg = JSON.parse(payload.toString())
+    // console.log('Hi i am here',msg);
+    console.log(`EVENT { event: ${msg.event},
+    time: ${new Date().toISOString()},
+    payload: { store: ${msg.payload.storeName},
+    orderID: ${msg.payload.orderId},
+    customer: ${msg.payload.customerName},
+    address: ${msg.payload.address} }
+}`);
     bordCastMsg(msg);
 });
 function bordCastMsg(msg) {
     let payload = JSON.stringify(msg);
     for (let id in connectionPool) {
+        // console.log(connectionPool[id]);
         connectionPool[id].write(payload);
     }
 }
 socket.on('close',()=>{
-
+    delete socketPool[id];
 })
 
 });
